6ab57a937ad340f522b40fe057ecae32c8ad3f49
Alex Pavlak <alexander.pavlak@mavs.uta.edu>
2019-04-06 13:53:39 -0500

    It built!

--- app/src/main/java/org/bodeen/recipebuilder/recyclers/MainActivityRecyclerAdapter.java
+++ app/src/main/java/org/bodeen/recipebuilder/recyclers/MainActivityRecyclerAdapter.java
@@ -3,16 +3,22 @@ import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 import androidx.recyclerview.widget.RecyclerView;
 import org.bodeen.recipebuilder.R;
+import org.bodeen.recipebuilder.model.Recipe;
+import org.w3c.dom.Text;
+
+import java.util.List;
 
 public class MainActivityRecyclerAdapter extends RecyclerView.Adapter<MainActivityRecyclerAdapter.ViewHolder> {
 
     public class ViewHolder extends RecyclerView.ViewHolder{
 
+        public TextView nameView;
         public ViewHolder(View recipeView){
             super(recipeView);
-
+            nameView = (TextView) itemView.findViewById(R.id.recipe_name);
         }
     }
 
@@ -34,11 +40,13 @@ public class MainActivityRecyclerAdapter extends RecyclerView.Adapter<MainActivi
     }
 
     @Override
-    public void onBindViewHOlder(MainActivityRecyclerAdapter.ViewHolder viewHolder, int position){
+    public void onBindViewHolder(MainActivityRecyclerAdapter.ViewHolder viewHolder, int position){
 
         Recipe recipe = mRecipes.get(position);
 
         //Create and initialize ui widgest
+        TextView textView = viewHolder.nameView;
+        textView.setText(recipe.getName());
     }
 
     @Override
