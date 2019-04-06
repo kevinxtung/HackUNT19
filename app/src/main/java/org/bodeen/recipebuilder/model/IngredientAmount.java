a8663d556c98629e9cf576080a88c1331de2ec28
Geetesh Kalakoti <kalakotig@gmail.com>
2019-04-06 16:09:32 -0500

    Add getters and setters for models

--- app/src/main/java/org/bodeen/recipebuilder/model/IngredientAmount.java
+++ app/src/main/java/org/bodeen/recipebuilder/model/IngredientAmount.java
@@ -3,6 +3,23 @@ package org.bodeen.recipebuilder.model;
 import org.bodeen.recipebuilder.model.entity.Ingredient;
 
 public class IngredientAmount {
+
     private Ingredient ingredient;
     private Amount amount;
+
+    public Ingredient getIngredient() {
+        return ingredient;
+    }
+
+    public void setIngredient(Ingredient ingredient) {
+        this.ingredient = ingredient;
+    }
+
+    public Amount getAmount() {
+        return amount;
+    }
+
+    public void setAmount(Amount amount) {
+        this.amount = amount;
+    }
 }
