a758f50c066cfef682010edae6d6256374015c22
Kevin Tung <35874751+kevinxtung@users.noreply.github.com>
2019-04-06 21:39:23 -0500

    Added API function for retrieving ingredient report (#10)

--- app/src/main/java/org/bodeen/recipebuilder/api/USDAService.kt
+++ app/src/main/java/org/bodeen/recipebuilder/api/USDAService.kt
@@ -1,6 +1,7 @@
 package org.bodeen.recipebuilder.api
 
 import org.bodeen.recipebuilder.api.model.FoodSearchResult
+import org.bodeen.recipebuilder.api.model.foodreport.FoodReportJSONObject
 import retrofit2.Call
 import retrofit2.http.GET
 import retrofit2.http.Query
@@ -9,4 +10,7 @@ interface USDAService {
 
     @GET("search/?format=json&sort=r&max=25")
     fun searchForIngredient(@Query("q") query: String, @Query("offset") offset: Int, @Query("api_key") apiKey: String = "DEMO_KEY"): Call<FoodSearchResult>
+
+    @GET("V2/reports?type=f&format=json")
+    fun retrieveIngredientReport(@Query("ndbno") query: String, @Query("api_key") apiKey: String = "DEMO_KEY"): Call<FoodReportJSONObject>
 }
