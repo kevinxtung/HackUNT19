1afc9058b64831b22ef3362eda529a29bc6b8e42
Kevin Tung <35874751+kevinxtung@users.noreply.github.com>
2019-04-06 22:42:04 -0500

    Api (#11)
    * Added API function for retrieving ingredient report
    
    * Added additional API calls for searching.

--- app/src/main/java/org/bodeen/recipebuilder/api/USDAService.kt
+++ app/src/main/java/org/bodeen/recipebuilder/api/USDAService.kt
@@ -11,6 +11,12 @@ interface USDAService {
     @GET("search/?format=json&sort=r&max=25")
     fun searchForIngredient(@Query("q") query: String, @Query("offset") offset: Int, @Query("api_key") apiKey: String = "DEMO_KEY"): Call<FoodSearchResult>
 
+    @GET("search/?format=json&sort=r&max=25")
+    fun searchForIngredientWithFoodGroup(@Query("q") query: String, @Query("offset") offset: Int, @Query("fg") group: String,@Query("api_key") apiKey: String = "DEMO_KEY"): Call<FoodSearchResult>
+
+    @GET("search/?format=json&sort=r&max=25&fg=0100&fg=0200&fg=0400&fg=0500&fg=0900&fg=1000&fg=1100&fg=1200&fg=1300&fg=1500&fg=1600&fg=1700&fg=2000")
+    fun searchForIngredientCulled(@Query("q") query: String, @Query("offset") offset: Int, @Query("fg") group: String,@Query("api_key") apiKey: String = "DEMO_KEY"): Call<FoodSearchResult>
+
     @GET("V2/reports?type=f&format=json")
-    fun retrieveIngredientReport(@Query("ndbno") query: String, @Query("api_key") apiKey: String = "DEMO_KEY"): Call<FoodReportJSONObject>
+    fun retrieveIngredientReport(@Query("ndbno") ndbno: String, @Query("api_key") apiKey: String = "DEMO_KEY"): Call<FoodReportJSONObject>
 }
